e26aa831104b7ac23b483bcf6e18f539fdcae321
Mason Egger <mason@masonegger.com>
2020-09-03 10:00:54 -0500

    fixng debug env var

--- rss_reader/settings.py
+++ rss_reader/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", True)
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 

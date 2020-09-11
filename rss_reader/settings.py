872e6172a5d2c921668759cb1f4665a4d5f56de4
Mason Egger <mason@masonegger.com>
2020-09-11 15:09:58 -0500

    fixing CORS

--- rss_reader/settings.py
+++ rss_reader/settings.py
@@ -163,9 +163,18 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
+
+if DEBUG is True:
+   CORS_ORIGIN_WHITELIST = [
+           "http://*",
+           "https://*"]
+else:
+    CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://localhost:8000",
-]
+    "https://127.0.0.1:3000",
+    "https://127.0.0.1:8000",
+    "https://localhost:3000",
+    "https://localhost:8000",]

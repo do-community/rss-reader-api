220750a603edea9142c1b0c0ee9ef20cdc3c8e31
Mason Egger <mason@masonegger.com>
2020-11-10 14:05:11 -0600

    Update settings.py

--- rss_reader/settings.py
+++ rss_reader/settings.py
@@ -173,7 +173,13 @@ if app_route is not None:
     FORCE_SCRIPT_NAME = "/{0}".format(os.path.relpath(app_route, '/'))
 
 # When deploying not at a subdirectory change this back to "/static/"
-STATIC_URL = "static/"
+
+# To make static files work, you need to deploy this app also as a static
+# site so the collect static can work. Then you'll change the build command
+# to `mv staticfiles _static` so they can be served and set the route for that 
+# static site to `/static`. now your static files will be served separately.
+
+STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
